8bb336ad5b894854afb6a0a4ee7a6e27dcd6e123
98erjuon <erikjuliusson98@hotmail.com>
2020-11-05 13:14:40 +0100

    Commit message

---
+++ Cars.java
@@ -0,0 +1,45 @@
+import java.awt.*;
+
+public class Cars {
+
+    public int nrDoors;
+    public double enginePower; // Engine power of the car
+    public double currentSpeed; // The current speed of the car
+    public Color color; // Color of the car
+    public String modelName; // The car model name
+
+
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+
+
+
+
+}
+
---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Volvo240 v1 = new Volvo240();
+        Saab95 s1 = new Saab95();
+
+
+    }
+}
